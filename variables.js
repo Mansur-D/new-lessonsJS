b1e309d7267684fa55fd50955b72fcd0587e67d8
Mansur <mansurdzukalaev@mail.ru>
2022-08-29 14:34:21 +0300

    Первый урок по Js

---
+++ variables.js
@@ -0,0 +1,20 @@
+let gameName;
+//название игры;
+let descriptionOfTheGame;
+//описание игры
+let gameVersion;
+//версия игры;
+let nameOfTheMainCharacterOfTheGame;
+//имя основного персонажа игры;
+let nameOfTheGameDeveloper;
+//имя разработчика игры;
+let theProgrammingLanguageInWhichTheGameIsWritten;
+//язык программирования, на которой написана игра;
+let theVersionOfTheProgrammingLanguageInWhichTheGameIsWritten;
+//версия языка программирования, на которой написана игра;
+let theNameOfTheMapOnWhichTheGameTakesPlace;
+//название карты, на которой происходят действия игры;
+let gameDevelopersWebsite;
+//сайт разработчика игры;
+let interfaceLanguage;
+//язык интерфейса;
